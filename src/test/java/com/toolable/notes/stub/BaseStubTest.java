678bb970f9442fdb6d6c797edea7a8cd9e6f619a
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2015-11-14 11:09:18 +0100

    [iss21][ADD] NotesStub class added

--- src/test/java/com/toolable/notes/stub/BaseStubTest.java
+++ src/test/java/com/toolable/notes/stub/BaseStubTest.java
@@ -19,22 +19,24 @@ import java.util.Vector;
 
 /**
  * Units tests for {@link BaseStub} and test utilities for derived classes
+ *
  * @author jonathan
  */
 public class BaseStubTest {
 
     /**
      * Assert that all method that a stub implements from a Lotus Notes interface raise a {@link RecycledObjectException} if the stub is recycled
+     *
      * @param stub
      */
-    public static void assertRecycledObjectExceptionsRaised(final BaseStub stub) {
+    public static void assertExceptionsRaisedOnRecycledObject(final BaseStub stub) {
         Preconditions.checkNotNull(stub);
 
         stub.setRecycled(true);
 
         for (Method method : getNotesMethods(stub.getClass())) {
             try {
-                method.invoke(stub, mock(method));
+                method.invoke(stub, mockArguments(method));
                 Assert.fail("The method \"" + method.getName() + "\" of the class \"" + stub.getClass().getSimpleName() + "\" should raise an exception if the object is recycled");
             } catch (InvocationTargetException e) {
                 Class<? extends Throwable> causeClass = e.getCause().getClass();
@@ -52,10 +54,11 @@ public class BaseStubTest {
 
     /**
      * Mock the parameters of a method
-     * @param method Method for which create mock parameters
+     *
+     * @param method Method for which create mockArguments parameters
      * @return Mocked parameters objects
      */
-    public static Object[] mock(Method method) {
+    public static Object[] mockArguments(Method method) {
         Class<?>[] types = method.getParameterTypes();
         Object[] parameters = new Object[types.length];
 
@@ -76,6 +79,7 @@ public class BaseStubTest {
 
     /**
      * Return the collection of method of an object that are implemented from a Lotus Notes interface
+     *
      * @param stubClass Object class
      * @return List of the method that the object class implements from a Lotus Notes interface
      */
@@ -96,6 +100,7 @@ public class BaseStubTest {
     /**
      * Base object should be recyclables.<br />
      * A second recycle call should raise an {@link RecycledObjectException}
+     *
      * @throws NotesException Unexpected exception
      */
     @Test
@@ -146,19 +151,19 @@ public class BaseStubTest {
      */
     @Test
     public void testRecycleObjectExceptionsRaised() {
-        BaseStubTest.assertRecycledObjectExceptionsRaised(new BaseStub());
+        BaseStubTest.assertExceptionsRaisedOnRecycledObject(new BaseStub());
     }
 
     /**
      * A list of Base object should be recycled by a call of {@link BaseStub#recycle(Vector)}
+     *
      * @throws NotesException
      */
     @Test
     public void testBulkRecycling() throws NotesException {
         Vector<BaseStub> slaves = new Vector<BaseStub>();
 
-        for( int i = 0 ; i < 10 ; i ++ )
-        {
+        for (int i = 0; i < 10; i++) {
             slaves.add(new BaseStub());
         }
 
@@ -168,9 +173,34 @@ public class BaseStubTest {
         // The object that made the call shouldn't be recycled
         Assert.assertFalse(master.isRecycled());
 
-        for (BaseStub slave : slaves)
-        {
+        for (BaseStub slave : slaves) {
             Assert.assertTrue(slave.isRecycled());
+        }
+    }
+
+    /**
+     * It should be easy to check if the recycling states of the NotesStubs
+     */
+    @Test
+    public void testAsserts() {
+        BaseStub stub = new BaseStub();
+
+        NotesStub.assertNotRecycled(stub);
+        try {
+            NotesStub.assertRecycled(stub);
+            Assert.fail("An exception should have been thrown");
+        } catch (AssertionError e) {
+            // Expected exception
+        }
+
+        stub.setRecycled(true);
+
+        NotesStub.assertRecycled(stub);
+        try {
+            NotesStub.assertNotRecycled(stub);
+            Assert.fail("An exception should have been thrown");
+        } catch (AssertionError e) {
+            // Expected exception
         }
     }
 }
