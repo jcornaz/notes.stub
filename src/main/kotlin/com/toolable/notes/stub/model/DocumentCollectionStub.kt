4ec505111d3cca1c293f0f3d17098630d784d359
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2016-06-19 19:52:09 +0200

    Comment DocumentCollectionStub

--- src/main/kotlin/com/toolable/notes/stub/model/DocumentCollectionStub.kt
+++ src/main/kotlin/com/toolable/notes/stub/model/DocumentCollectionStub.kt
@@ -5,11 +5,17 @@ import com.toolable.notes.stub.utils.lazyParent
 
 /**
  * Stub for [lotus.domino.DocumentCollection]
+ *
+ * @constructor Create a new document collection
+ * @property documents Documents contained in the collection
  */
 data class DocumentCollectionStub(val documents: MutableList<DocumentStub> = emptyList<DocumentStub>().toMutableList()) : BaseStub<DocumentCollectionImpl>, MutableList<DocumentStub> by documents {
     override val implementation = DocumentCollectionImpl(this)
     override var isRecycled = false
 
+    /**
+     * Parent databse
+     */
     var database by lazyParent(
             { DatabaseStub() },
             { collections = collections.minusElement(this@DocumentCollectionStub) },
