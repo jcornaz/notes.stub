86f8af4d5ba76321d2b3debcb734c418e4947956
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2016-06-19 09:10:47 +0200

    Fix comment of exceptions

--- src/main/kotlin/com/toolable/notes/stub/exception/NonExistantDocumentException.kt
+++ src/main/kotlin/com/toolable/notes/stub/exception/NonExistantDocumentException.kt
@@ -2,11 +2,5 @@ package com.toolable.notes.stub.exception
 
 /**
  * Exception raised when a Lotus Document is asked but does not exist
- *
- * @author jonathan
- *
- * @see Base.recycle
- * @see BaseStub.setRecycled
- * @see BaseStub.isRecycled
  */
 class NonExistantDocumentException : Exception()
--- src/main/kotlin/com/toolable/notes/stub/exception/RecycledObjectException.kt
+++ src/main/kotlin/com/toolable/notes/stub/exception/RecycledObjectException.kt
@@ -2,11 +2,5 @@ package com.toolable.notes.stub.exception
 
 /**
  * Exception raised when a Lotus Notes method as been called on a recycled object
- *
- * @author jonathan
- *
- * @see Base.recycle
- * @see BaseStub.setRecycled
- * @see BaseStub.isRecycled
  */
 class RecycledObjectException : RuntimeException()
