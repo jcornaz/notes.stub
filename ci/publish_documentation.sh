4bbb009d7304384975e045224f389e3f0e691a9f
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2016-06-20 17:43:39 +0200

    Remove dokka from the required tasks before check

--- ci/publish_documentation.sh
+++ ci/publish_documentation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+app_name='com.toolable.notesstub'
 version='0.1-SNAPSHOT'
 
 if [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
@@ -11,10 +12,13 @@ if [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && [ "$TRAVIS_PULL_REQUEST" == "fal
   git config --global user.name "travis-ci"
   git clone --branch=gh-pages https://${GH_TOKEN}@github.com/$GH_REPOSITORY.git gh-pages &> /dev/null
 
+  echo "Generating doc..."
+  ./gradlew dokka
+
   echo "Copying doc..."
   rm -rf gh-pages/doc/*-SNAPSHOT
   mkdir gh-pages/doc/$version
-  cp -r ./build/dokka/com.toolable.notesstub/* gh-pages/doc/$version
+  cp -r ./build/dokka/$app_name/* gh-pages/doc/$version
 
   echo "Pushing a new version of gh-pages..."
   cd gh-pages
